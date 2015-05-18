de778093109c6d09aea6c9324843d2130eac979a
hikarock <i@hika69.com>
2015-05-18 23:30:30 +0900

    node-configの設定ファイルを追加

--- src/jsx/fetchers/playlists.jsx
+++ src/jsx/fetchers/playlists.jsx
@@ -1,13 +1,14 @@
 'use strict';
 import request from 'superagent';
+import config from 'config';
 
-// mock
-const REMP_API = 'http://localhost:8190';
+let api = config.get('api');
+const REMP_API = `http://${api.host}:${api.port}`;
 
 export default {
   name: 'playlists',
 
-  read(req, resource, params, config, callback) {
+  read(req, resource, params, conf, callback) {
 
     let id = params.id || '';
 
